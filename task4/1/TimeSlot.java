9210b1e7a5e21764e958d7bc10a045d9259a2917
gama1726 <gama17one@gmail.com>
2025-10-31 22:18:36 +0300

    feat:расширяем функционал и проверяем накладку таймслотов

--- task4/1/TimeSlot.java
+++ task4/1/TimeSlot.java
@@ -19,6 +19,15 @@ public class TimeSlot {
     public LocalDateTime getEnd(){
         return end;
     }
+    //пересекаются ли заказы с другим слотом
+    public boolean overlaps(TimeSlot other){
+        return this.start.isBefore(other.end) && other.getStart().isBefore(this.end);
+
+    }
+    //момент времени попадает внутрь слота?
+    public boolean contains(LocalDateTime t){
+        return(!t.isBefore(start) && t.isBefore(end));
+    }
     @Override
     public String toString(){
         return "Начало слота : " + start + ",Конец: " + end;
